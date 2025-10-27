197bb6ce54585f9fdd5cc5c72c55dc841ce45e80
zane <zhangyinlei1990@gmail.com>
2025-10-27 21:36:39 +0800

    merge/monitor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header({ showBackButton = false }: HeaderProps) {
 
   return (
     <header className="w-full border-b border-slate-200 bg-white/50 h-20">
-      <div className="w-full max-w-[1400px] mx-auto flex items-center justify-between py-2 px-4">
+      <div className="w-full h-full max-w-[1400px] mx-auto flex items-center justify-between px-4">
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-2">
             <div className="w-10 h-10 rounded-full bg-linear-to-br from-orange-400 to-pink-500" />
